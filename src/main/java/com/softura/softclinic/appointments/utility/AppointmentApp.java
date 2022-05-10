ba62db5bbc2d73d5f8422429cb8fa1b88da308e9
eswaribala <parameswaribala@gmail.com>
2022-05-10 18:37:39 +0530

    Updated

--- src/main/java/com/softura/softclinic/appointments/utility/AppointmentApp.java
+++ src/main/java/com/softura/softclinic/appointments/utility/AppointmentApp.java
@@ -10,21 +10,52 @@ import java.util.Scanner;
 public class AppointmentApp {
 
     public static  void main(String[] args){
-
-        Appointment appointment=new Appointment();
         Scanner scanner=new Scanner(System.in);
         //read the values
-        System.out.println("Enter Patient Mobile No");
-        appointment.setPatientMobileNo(scanner.nextLong());
+        System.out.println("Enter number of appointments....");
+        int appts=scanner.nextInt();
         scanner.nextLine();
-        System.out.println("Enter Date of appointment yyyy-mm-dd");
-         appointment.setDoa(LocalDate.parse(scanner.nextLine()));
-        System.out.println("Enter Time of appointment HH:MM");
-        appointment.setTime(scanner.nextLine());
-        System.out.println("Enter Doctor Name");
-        appointment.setDoctorName(scanner.nextLine());
+        //array of appointments
+        Appointment[] appointments=new Appointment[appts];
+        //ref variable
+        Appointment appointment=null;
+        //loop with variables
+        for(int i=0;i<appointments.length;i++) {
 
-        //view the details
-       System.out.println(appointment);
+            //appointment object
+             appointment = new Appointment();
+             System.out.println("Enter Patient Mobile No");
+             appointment.setPatientMobileNo(scanner.nextLong());
+             scanner.nextLine();
+             System.out.println("Enter Date of appointment yyyy-mm-dd");
+             appointment.setDoa(LocalDate.parse(scanner.nextLine()));
+             System.out.println("Enter Time of appointment HH:MM");
+             appointment.setTime(scanner.nextLine());
+             System.out.println("Enter Doctor Name");
+             appointment.setDoctorName(scanner.nextLine());
+
+            //add object to array
+             appointments[i]=appointment;
+
+         }
+
+          //view the appointment list
+        //enhanced for loop
+        for(Appointment appointmentObj : appointments)
+            System.out.println(appointmentObj);
+
+       //compare the appointment status
+
+        LocalDate currentDate=LocalDate.now();
+         //conditional statement
+        if((currentDate.equals(appointments[0].getDoa()))||(currentDate.isBefore(appointments[0].getDoa()))) {
+            System.out.println("Result.....");
+            System.out.println("Valid Appointment");
+        }
+        else {
+            System.out.println("Result.....");
+            System.out.println("Invalid Appointment...");
+        }
+
     }
 }
